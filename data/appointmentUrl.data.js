1a3c478935467dffe8f02e9a6d86b7bb39b7e3f4
Sumit Ghewade <sumitghewade@Sumits-MacBook-Pro.local>
2021-10-21 18:45:16 -0400

    added location details for renewal appointment

--- data/appointmentUrl.data.js
+++ data/appointmentUrl.data.js
@@ -15,6 +15,46 @@ const mvcData = [
         locationName: 'SouthPlainfield',
         locationNumber: '109',
       },
+      {
+        locationName: 'Bakers Basin',
+        locationNumber: '101',
+      },
+      {
+        locationName: 'Bayonne',
+        locationNumber: '102',
+      },
+      {
+        locationName: 'Camden',
+        locationNumber: '104',
+      },
+      {
+        locationName: 'Cardiff',
+        locationNumber: '105',
+      },
+      {
+        locationName: 'Delanco',
+        locationNumber: '107',
+      },
+      {
+        locationName: 'Eatontown',
+        locationNumber: '108',
+      },
+      {
+        locationName: 'Elizabeth',
+        locationNumber: '261',
+      },
+      {
+        locationName: 'Flemington',
+        locationNumber: '111',
+      },
+      {
+        locationName: 'Freehold',
+        locationNumber: '113',
+      },
+      {
+        locationName: 'Lodi',
+        locationNumber: '114',
+      },
     ],
   },
   {
@@ -33,6 +73,46 @@ const mvcData = [
         locationName: 'SouthPlainfield',
         locationNumber: '239',
       },
+      {
+        locationName: 'Bakers Basin',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Bayonne',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Camden',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Cardiff',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Delanco',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Eatontown',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Elizabeth',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Flemington',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Freehold',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Lodi',
+        locationNumber: '109',
+      },
     ],
   },
   {
@@ -50,6 +130,46 @@ const mvcData = [
       {
         locationName: 'SouthPlainfield',
         locationNumber: '193',
+      },
+      {
+        locationName: 'Bakers Basin',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Bayonne',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Camden',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Cardiff',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Delanco',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Eatontown',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Elizabeth',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Flemington',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Freehold',
+        locationNumber: '109',
+      },
+      {
+        locationName: 'Lodi',
+        locationNumber: '109',
       },
     ],
   },
